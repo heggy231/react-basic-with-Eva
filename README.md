250940cd505e1e44fd1cbc150777fefdeb26552f
Heggy Castaneda <heggyy@gmail.com>
2021-07-12 05:52:22 -0400

    update comments

--- README.md
+++ README.md
@@ -676,3 +676,46 @@ ReactDOM.render(
   document.getElementById("root")
 );
 ```
+
+```jsx
+function GitHubUsers() {
+  // fetching data from https://api.github.com/users
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // fetching github users data
+    const url = `https://api.github.com/users`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(setData)
+      .catch(error => console.error(error));
+  }, []);
+
+  if(data) {
+    // returns html string of massaged data from api call
+    return (
+      data.map((user, index) => 
+        <div key={index}>
+          <h1>User Login: {user.login}</h1>
+          <img src={user.avatar_url} width={70} alt={user.login} />
+          <a href={user.html_url} target="_blank" rel="noreferrer"><h3>{user.login}'s Github Homepage 🌟</h3></a>
+        </div>
+      )
+    );
+  }
+
+  // if there is no user data returned from api call
+  return null;
+}
+
+// pop github users: fabpot andrew taylorotwell
+function App() {
+  return <GitHubUsers />
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById("root")
+);
+```
--- src/index.js
+++ src/index.js
@@ -6,30 +6,32 @@ import "./index.css";
 function GitHubUsers() {
   // fetching data from https://api.github.com/users
   const [data, setData] = useState(null);
-
+  
+  // introduce side-effect fetch api call but wrap
+  //  inside of useEffect to decouple, second optional 
+  //  empty dependency array to only render when component mounts to virtual DOM
   useEffect(() => {
-    // fetching github users data
-    const url = `https://api.github.com/users`;
-
+    // fetching github users data`https://api.github.com/users`;
+    const url = 'https://api.github.com/users';
     fetch(url)
       .then(response => response.json())
       .then(setData)
       .catch(error => console.error(error));
   }, []);
-
+  
   if(data) {
-    // returns html string of massaged data from api call
+    // console.log('data', data);
     return (
       data.map((user, index) => 
         <div key={index}>
           <h1>User Login: {user.login}</h1>
-          <img src={user.avatar_url} width={70} alt={user.login} />
+          <img src={user.avatar_url} alt={user.login} width={70} />
           <a href={user.html_url} target="_blank" rel="noreferrer"><h3>{user.login}'s Github Homepage 🌟</h3></a>
         </div>
       )
     );
   }
-
+  
   // if there is no user data returned from api call
   return null;
 }
