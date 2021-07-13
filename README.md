526add4df99d4cdbd8ee91c4fe8c79b3d395c62a
Heggy Castaneda <heggyy@gmail.com>
2021-07-13 09:03:13 -0400

    quiz start

--- README.md
+++ README.md
@@ -721,3 +721,34 @@ ReactDOM.render(
 ```
 
 ![result](./asset/userDatalog.png)
+
+## Reducer
+- mini goal: Checked or not checked text showing up
+
+"I want" : "no I don't"
+
+
+```jsx
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+const Checkbox = () => {
+  const [checked, setChecked] = useState(false);
+  return (
+    <>
+      <input 
+      type="checkbox" 
+      value={checked} 
+      onChange={() => setChecked(checked => !checked)} />
+      {checked ? "I want" : "no I don't"}
+    </>
+  );
+}
+
+ReactDOM.render(
+  <Checkbox />, 
+  document.getElementById("root")
+);
+```
--- src/index.js
+++ src/index.js
@@ -1,47 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 
-function GitHubUsers() {
-  // fetching data from https://api.github.com/users
-  const [data, setData] = useState(null);
-  
-  // introduce side-effect fetch api call but wrap
-  //  inside of useEffect to decouple, second optional 
-  //  empty dependency array to only render when component mounts to virtual DOM
-  useEffect(() => {
-    // fetching github users data`https://api.github.com/users`;
-    const url = 'https://api.github.com/users';
-    fetch(url)
-      .then(response => response.json())
-      .then(setData)
-      .catch(error => console.error(error));
-  }, []);
-  
-  if(data) {
-    // console.log('data', data);
-    return (
-      data.map((user, index) => 
-        <div key={index}>
-          <h1>User Login: {user.login}</h1>
-          <img src={user.avatar_url} alt={user.login} width={70} />
-          <a href={user.html_url} target="_blank" rel="noreferrer"><h3>{user.login}'s Github Homepage 🌟</h3></a>
-        </div>
-      )
-    );
-  }
-  
-  // if there is no user data returned from api call
-  return null;
-}
-
-// pop github users: fabpot andrew taylorotwell
-function App() {
-  return <GitHubUsers />
+const Checkbox = () => {
+  const [checked, setChecked] = useState(false);
+  return (
+    <>
+      <input 
+      type="checkbox" 
+      value={checked} 
+      onChange={() => setChecked(checked => !checked)} />
+      {checked ? "I want" : "no I don't"}
+    </>
+  );
 }
 
 ReactDOM.render(
-  <App />, 
+  <Checkbox />, 
   document.getElementById("root")
 );
