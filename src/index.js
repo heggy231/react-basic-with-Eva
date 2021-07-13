07f00944da02dda34872f010e90db017bf62d979
Heggy Castaneda <heggyy@gmail.com>
2021-07-13 09:03:59 -0400

    begin quiz checkbox

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import "./index.css";
 
 
 const Checkbox = () => {
-  const [checked, setChecked] = useState(false);
-  return (
-    <>
-      <input 
-      type="checkbox" 
-      value={checked} 
-      onChange={() => setChecked(checked => !checked)} />
-      {checked ? "I want" : "no I don't"}
-    </>
-  );
+  
 }
 
 ReactDOM.render(
